17057acb11a9caf0a7c18a1bdc927a726c6ae618
Robbert <mail@robbertbroersma.nl>
2021-05-14 10:43:13 +0000

    Deploying to gh-pages from @ nl-design-system/amsterdam-design-tokens@efeb5c0297014ad413479a08bb3dca803a9be738 🚀

---
+++ index.js
@@ -0,0 +1,57 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 14 May 2021 10:43:12 GMT
+ */
+
+export const colorRed40 = "hsl(0 100% 37%)";
+export const colorRed50 = "hsl(0 100% 46%)";
+export const colorBlue25 = "hsl(212 100% 24%)";
+export const colorBlue30 = "hsl(213 100% 30%)";
+export const colorWhite = "hsl(0 0% 100%)";
+export const colorBlack = "hsl(0 0% 0%)";
+export const colorGray0 = "hsl(0 0% 0%)";
+export const colorGray20 = "hsl(0 0% 20%)";
+export const colorGray50 = "hsl(0 0% 46%)";
+export const colorGray70 = "hsl(0 0% 71%)";
+export const colorGray90 = "hsl(0 0% 90%)";
+export const colorGray95 = "hsl(0 0% 96%)";
+export const colorGray100 = "hsl(0 0% 0%)";
+export const colorGreen30 = "hsl(143 100% 31%)";
+export const colorAmber50 = "hsl(46 99% 54%)";
+export const colorPurple30 = "hsl(315 100% 31%)";
+export const colorPink50 = "hsl(326 100% 45%)";
+export const colorOrange50 = "hsl(34 100% 50%)";
+export const colorLightgreen40 = "hsl(66 100% 41%)";
+export const colorLightblue50 = "hsl(200 100% 46%)";
+export const feedbackError = "hsl(0 100% 46%)";
+export const validationValid = "hsl(143 100% 31%)";
+export const validationInvalid = "hsl(0 100% 46%)";
+export const focusOutlineColor = "hsl(46 99% 54%)";
+export const documentColor = "hsl(0 0% 0%)";
+export const documentFontWeight = "400";
+export const documentLineHeight = "22px";
+export const headingFontWeight = "700";
+export const heading1FontSize = "24px";
+export const heading1LineHeight = "30px";
+export const heading1MarginBlockEnd = "0";
+export const heading2FontSize = "20px";
+export const heading2LineHeight = "28px";
+export const heading2MarginBlockEnd = "8px";
+export const heading3FontSize = "20px";
+export const heading3LineHeight = "28px";
+export const heading3MarginBlockEnd = "8px";
+export const heading4FontSize = "18px";
+export const heading4LineHeight = "25px";
+export const heading4MarginBlockEnd = "8px";
+export const heading5FontSize = "18px";
+export const heading5LineHeight = "25px";
+export const heading5MarginBlockEnd = "8px";
+export const heading6FontSize = "16px";
+export const heading6LineHeight = "20px";
+export const heading6MarginBlockEnd = "0";
+export const paragraphFontSize = "16px";
+export const paragraphLineHeight = "22px";
+export const paragraphMarginBlockEnd = "0";
+export const listItemMarginBlockEnd = "8px";
+export const smallFontSize = "14px";
+export const smallLineHeight = "18px";
